151503c8afdf2bb9a75ed40300c74cdaa3a59791
Gypsynkov <mambakla@gmail.com>
2020-02-24 22:18:19 +0300

    abs task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    n = n + "";
+    console.log(String(n).split('').reverse());
+	return n.split("").reverse().join("");
 }
+
+  
+
